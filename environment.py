c584bb185f694461954faa289e904377d435e244
Esteban Henao <estebanhenaov@gmail.com>
2021-11-02 17:54:27 -0500

    Microservicio para generacion de notificaciones

---
+++ environment.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 18:01:35 2021
+
+@author: ESTEBAN
+"""
+
+import os
+
+os.environ["SENDGRID_API_KEY"]=""
+os.environ["SECURITY_HASH"]=""
+os.environ["email_from"]=""
+os.environ["TWILIO_ACCOUNT_SID"]=""
+os.environ["TWILIO_AUTH_TOKEN"]=""
---
+++ servidor.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 18:00:09 2021
+
+@author: ESTEBAN
+"""
+
+from flask import Flask
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from flask import request
+from twilio.rest import Client
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/correo")
+def enviarCorreo():
+    destino = request.args.get("destino")
+    asunto = request.args.get("asunto")
+    mensaje = request.args.get("mensaje")
+    hashString = request.args.get("hash")
+    if  hashString == os.environ.get("SECURITY_HASH"):
+        message = Mail(
+            from_email=os.environ.get("email_from"),
+            to_emails=destino,
+            subject=asunto,
+            html_content=mensaje)
+        try:
+            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            response = sg.send(message)
+            print("enviado")
+            return "OK"
+        except Exception as e:
+            print(e.message)
+            return "KO"
+    else:
+        print("Hash error")
+        return "KO"
+
+@app.route("/sms")
+def enviarSms():
+    destino = request.args.get("destino")
+    mensaje = request.args.get("mensaje")
+    hashString = request.args.get("hash")
+    if  hashString == os.environ.get("SECURITY_HASH"):
+        try:
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            client = Client(account_sid, auth_token)
+            
+            message = client.messages \
+                            .create(
+                                 body=mensaje,
+                                 from_="+13187271933",
+                                 to="+57"+destino
+                             )
+            
+            print(message.sid)
+            print("enviado sms")
+            return "OK"
+        except Exception as e:
+            print(e.message)
+            return "KO"
+    else:
+        print("Hash error")
+        return "KO"
+
+
+if __name__ == "__main__":
+    app.run()
+    
